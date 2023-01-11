ef8cc91fc92afd33dff1d96e8e1537f43194eebb
Rafael Girão <git@rafael.ovh>
2023-01-11 16:28:27 +0000

    Update function signatures :|

--- manager/manager.c
+++ manager/manager.c
@@ -17,9 +17,11 @@ Imprime a resposta e termina.
 
 static void print_usage() {
     fprintf(stderr, "usage: \n"
-                    "   manager <register_pipe_name> create <box_name>\n"
-                    "   manager <register_pipe_name> remove <box_name>\n"
-                    "   manager <register_pipe_name> list\n");
+                    "   manager <register_pipe_name> <pipe_name> create <box_name>\n"
+                    "   manager <register_pipe_name> <pipe_name> remove <box_name>\n"
+                    "   manager <register_pipe_name> <pipe_name> list\n");
+
+                    
 }
 
 int list_boxes() {
--- publisher/pub.c
+++ publisher/pub.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
-    fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
+    fprintf(stderr, "usage: pub <register_pipe_name> <pipe_name> box_name>\n");
     WARN("unimplemented"); // TODO: implement
     return -1;
 }
