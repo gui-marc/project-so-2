5c96c2fb02ed3d381199951851d61d6e47ebfecf
Rafael Girão <git@rafael.ovh>
2023-01-13 13:00:53 +0000

    :sparkles: mbroker: Implement box creation

--- mbroker/requests.c
+++ mbroker/requests.c
@@ -1,3 +1,8 @@
+#include "requests.h"
+#include "betterassert.h"
+#include "logging.h"
+#include "operations.h"
+#include "protocols.h"
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
@@ -6,10 +11,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-#include "logging.h"
-#include "protocols.h"
-#include "requests.h"
 
 void *listen_for_requests(void *queue) {
     while (true) {
@@ -23,19 +24,19 @@ void *listen_for_requests(void *queue) {
 
 void parse_request(u_int8_t request_code, void *protocol) {
     switch (request_code) {
-    case CODE_REGISTER_PUBLISHER:
+    case REGISTER_PUBLISHER:
         register_publisher(protocol);
         break;
-    case CODE_REGISTER_SUBSCRIBER:
+    case REGISTER_SUBSCRIBER:
         register_subscriber(protocol);
         break;
-    case CODE_CREATE_BOX_REQUEST:
+    case CREATE_BOX_REQUEST:
         create_box(protocol);
         break;
-    case CODE_REMOVE_BOX_REQUEST:
+    case REMOVE_BOX_REQUEST:
         remove_box(protocol);
         break;
-    case CODE_LIST_BOXES_REQUEST:
+    case LIST_BOXES_REQUEST:
         list_boxes(protocol);
         break;
     default:
@@ -55,8 +56,30 @@ void register_subscriber(void *protocol) {
 }
 
 void create_box(void *protocol) {
-    (void)protocol;
-    WARN("not implemented\n"); // Todo: implement me
+    create_box_proto_t *request = (create_box_proto_t *)protocol;
+
+    create_box_response_proto_t *response =
+        calloc(1, sizeof(create_box_response_proto_t));
+    ALWAYS_ASSERT(response != NULL, "Call to calloc failed.");
+    response->return_code = REMOVE_BOX_RESPONSE;
+
+    int pipe_fd = open_pipe(request->client_named_pipe_path);
+
+    // tfs_lookup isn't exposed in headers,
+    //  so use tfs_open for this check
+    int fd = tfs_open(request->box_name, 0);
+    if (fd != -1) {
+        snprintf(response->error_msg, MSG_SIZE, ERR_BOX_ALREADY_EXISTS);
+        tfs_close(fd);
+        send_proto_string(pipe_fd, REMOVE_BOX_RESPONSE, response);
+        return;
+    }
+    // Actually create the new file for the box
+    fd = tfs_open(request->box_name, TFS_O_CREAT);
+    if (fd == -1) {
+        snprintf(response->error_msg, MSG_SIZE, ERR_BOX_CREATION);
+    }
+    send_proto_string(pipe_fd, REMOVE_BOX_RESPONSE, response);
 }
 
 void remove_box(void *protocol) {
